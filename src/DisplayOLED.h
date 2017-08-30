44227e796bdbe8523990297e31760427b26d63ac
michaln <michal.nezerka@gmail.com>
2017-08-30 07:23:36 +0200

    Screens on OLED display

--- src/DisplayOLED.h
+++ src/DisplayOLED.h
@@ -8,13 +8,18 @@
 #include "TimeProvider.h"
 #include "images.h"
 
+// time to show each screen in seconds
+#define SCREEN_DURATION 5
+#define SCREEN_COUNT 2
+
 class DisplayOLED: public DeviceStateListener
 {
     private:
         // declare OLED display
         SSD1306 *oled;
 		String lastState;
-
+		unsigned long lastScreenChange;
+		unsigned int screen = 0;
     public:
 
     DisplayOLED()
@@ -26,6 +31,7 @@ class DisplayOLED: public DeviceStateListener
         // D3 -> SDA
         // D5 -> SCL
         oled = new SSD1306(0x3c, D3, D5);
+		lastScreenChange = millis();
     }
   
     void begin()
@@ -46,9 +52,6 @@ class DisplayOLED: public DeviceStateListener
 
     void processReadings(Sensors &sensors)
     {
-        //oled->clear();
-        //lcd->home();
-
         // get all sensor readings and generate appropriate HTML representation
         for(int i = 0; i < sensors.getSensors()->size(); i++)
         {
@@ -77,17 +80,32 @@ class DisplayOLED: public DeviceStateListener
         oled->setTextAlignment(TEXT_ALIGN_RIGHT);
         oled->drawString(128, 0, TimeProvider::getInstance().getTimeStr());
 
-        oled->setTextAlignment(TEXT_ALIGN_LEFT);
+		// check if screen sould be switched to next one
+		if ((millis() - lastScreenChange) > (SCREEN_DURATION * 1000))
+		{
+			screen = (screen + 1) % SCREEN_COUNT;
+			lastScreenChange = millis();
+		}
 
-		// wifi information
+		switch (screen)
+		{
+			case 0:
+				oled->setTextAlignment(TEXT_ALIGN_LEFT);
 
-		// device state
-		oled->drawString(0, 10, "State: " + lastState);
+				// device state
+				oled->drawString(0, 10, "State: " + lastState);
 
-		// wifi information
-		oled->drawString(0, 20, "Wifi: " + WiFi.SSID());
-		oled->drawString(0, 30, "IP: " + WiFi.localIP().toString());
-		oled->drawString(0, 40, "Wifi strength: " + String(WiFi.RSSI()) + " dBm");
+				// wifi information
+				oled->drawString(0, 20, "Wifi: " + WiFi.SSID());
+				oled->drawString(0, 30, "IP: " + WiFi.localIP().toString());
+				oled->drawString(0, 40, "Wifi strength: " + String(WiFi.RSSI()) + " dBm");
+				break;
+
+			case 1:
+				oled->setTextAlignment(TEXT_ALIGN_LEFT);
+				oled->drawString(0, 10, "Meter readings");
+				break;
+		}
 
         oled->display();
     }
